caf4769461100091c8a802b3d76b41e565df6683
HybridDog <ovvv@web.de>
2016-01-10 16:42:18 +0100

    :cat2:

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-minetest.log("action", "[BobBlocks By minetest@rabbibob.com] Version 0.0.8 loading....")
+minetest.log("info", "[BobBlocks By minetest@rabbibob.com] Version 0.0.8 loading....")
 dofile(minetest.get_modpath("bobblocks") .. "/blocks.lua")
 dofile(minetest.get_modpath("bobblocks") .. "/health.lua")
 dofile(minetest.get_modpath("bobblocks") .. "/trap.lua")
-minetest.log("action", "[BobBlocks By minetest@rabbibob.com] Version 0.0.8 loaded!")
+minetest.log("info", "[BobBlocks By minetest@rabbibob.com] Version 0.0.8 loaded!")
